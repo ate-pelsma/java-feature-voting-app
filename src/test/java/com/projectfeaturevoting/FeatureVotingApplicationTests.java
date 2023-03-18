30b27f282869bfae89716e4827b06360861fc320
ate-pelsma <atepelsma@gmail.com>
2023-03-18 12:54:07 +0100

    first commit

---
+++ src/test/java/com/projectfeaturevoting/FeatureVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectfeaturevoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
